f151b2c5eeb11e93459048a97236032824cdd94a
ohendarko <ohendarko@gmail.com>
2024-01-01 22:47:23 +0000

    Added docs for password program

--- Automate/pass.py
+++ Automate/pass.py
@@ -1,4 +1,57 @@
 #!/usr/bin/python3
+"""
+Adapted from AUTOMATE THE BORING STUFF WITH PYTHON
+By Al Sweigart
+Page 136-138
+
+Original Code:
+===============================================
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+            'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+            'luggage': '12345'}
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+    print('Usage: py pw.py [account] - copy account password')
+    sys.exit()
+account = sys.argv[1] # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+print('There is no account named ' + account)
+===============================================
+
+Issues and What can be improved upon:
+==================================================
+1. Passwords are predifined and can be only be added as part of the code.
+What if there was a part of the code that allowed you to update
+from the running instance of the program without actually opening up the module
+to update the dictionary with the password.
+
+Added/Modified:
+=============
+a. If password not found, initiate an addition of password for unknown account.
+b. Moved password dictionary to a separate json file. Program searches
+from this file instead of prestated dict in code.
+c. Program saves new password to json file. Can be retrieved anytime.
+
+PENDING features:
+==========================================
+i. Remove password
+ii. Change password
+
+REQUIRES basic knowledge in:
+===============================================
+1. Flow control; if, while, else loops
+2. Dictionaries data structure
+3. Functions
+4. Command line arguments
+5. Modules: json, pyperclip, sys
+6. File i/o
+7. Print formatting
+"""
 import sys
 import pyperclip
 import json
